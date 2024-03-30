ab12931f69d4bad1ac9e0274a74b2d015b1b722c
son hoang <myanhm55@gmail.com>
2024-03-30 14:45:38 +0700

    test p2.2 rebase 1

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("p1");
+        System.out.println("p2");
+        System.out.println("p2.2");
+    }
+}
